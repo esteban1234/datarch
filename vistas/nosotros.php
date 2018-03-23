5fb1bd023f5dad7a41d672431c93761b506e0c49
esteban1234 <tva_libra@hotmail.com>
2018-03-22 18:20:32 -0600

    nosotros a medias

--- vistas/nosotros.php
+++ vistas/nosotros.php
@@ -25,6 +25,81 @@
             </div>
         </div>
     </div>
+
+
+<div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-3 cuadro">
+            <div class="iconos">
+              <i class="fas fa-cart-plus fa-5x"></i>
+            </div>
+            <h2 class="text-center">Sabemos lo que necesita</h2>
+            <p class="text-center">Si amas tu auto y sabes lo que se necesita para que este al MÁXIMO. ¡Somos la empresa que te entiende!</p>
+          </div>
+          <div class="col-md-3 cuadro">
+            <div class="iconos">
+              <i class="fas fa-map-marker-alt fa-5x"></i>
+            </div>
+            <h2 class="text-center">Todo en su lugar</h2>
+            <p class="text-center">Todo lo que requieres para que tu auto se vea como quieres, bajo un mismo techo.
+            No necesitaras visitar mas lugares.</p>
+          </div>
+          <div class="col-md-3 cuadro">
+            <div class="iconos">
+              <i class="fas fa-cogs fa-5x"></i>
+            </div>
+            <h2 class="text-center">Accesorios</h2>
+            <p class="text-center">En los accesorios se ven los buenos cambios. Y aquí tenemos los accesorios que estás buscando para tu auto. ¡Que necesitas!</p>
+          </div>
+          <div class="col-md-3 cuadro">
+            <div class="iconos">
+              <i class="fas fa-car fa-5x"></i>
+            </div>
+            <h2 class="text-center">Piezas y refacciones</h2>
+            <p class="text-center">¿Necesitas una pieza especifica para tu auto? No pierdas tiempo y ven directo con nosotros. Aquí seguro la encuentras.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="" style="padding: 5% 0%; background: #CCCCCC;">
+        <h1 class="texton">Somos una empresa con experiencia en la comercialización al mayoreo de partes de repuestos automotrices y servicio pesado, tanto nacionales como importadas. <br><br>
+        Te ofrecemos autopartes y accesorios que mejoren la apariencia de tu automóvil o su funcionamiento. Buscamos proveer lo más actualizado de accesorios de alta calidad enfocados principalmente a la seguridad y cuidado del automóvil, ofreciendo productos en constante renovación de acuerdo con los avances tecnológicos.
+
+        Somos distribuidores, mayoristas y nuestros productos se pueden aplicar en toda clase y tipos de vehículos.</h1>
+         <div class="text-center" style="margin-top: 5%;">
+                <a href="vistas/contacto.php" class="btn btn-danger btn-outline btn-lg">CONTACTANOS</a>
+            </div>
+    </div>
+
+    <div class="section" style="padding: 5% 0%;">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">MISION</h2>
+            <p class="text-center">Vender autopartes ofreciendo seguridad, variedad, calidad y confianza en nuestros productos y en las compras que realice, asegurando garantías en tiempo de entrega y costo de envío.</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">VISION</h2>
+            <p class="text-center">Ser el mejor distribuidor de autopartes, manteniendo al 100% de nuestros clientes satisfechos.</p>
+          </div>
+          <div class="col-md-4">
+            <img src="http://pingendo.github.io/pingendo-bootstrap/assets/placeholder.png"
+            class="img-responsive">
+            <h2 class="text-center">VALORES</h2>
+            <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisici elit,
+              <br>sed eiusmod tempor incidunt ut labore et dolore magna aliqua.
+              <br>Ut enim ad minim veniam, quis nostrud</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
+
 <a class="go-top" href="#">Subir</a>
 <?php include 'pie.php' ?>
 
